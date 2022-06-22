eea4f10d938f14f836ebd4734ce1eec65718ffe8
reduckted <reduckted@outlook.com>
2022-06-22 19:04:04 +1000

    Prevented keystrokes from being intercepted when there are multiple selections that are all empty.

--- source/AutoSurround/AutoSurroundCommandHandler.cs
+++ source/AutoSurround/AutoSurroundCommandHandler.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Commanding;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
@@ -50,8 +50,9 @@ public class AutoSurroundCommandHandler : ICommandHandler<TypeCharCommandArgs> {
     public bool ExecuteCommand(TypeCharCommandArgs args, CommandExecutionContext executionContext) {
         if (!args.TextView.Selection.IsEmpty) {
             if (CharacterPairs.TryGetValue(args.TypedChar, out char closing)) {
-                SurroundWith(args.TypedChar, closing, args.TextView);
-                return true;
+                if (SurroundWith(args.TypedChar, closing, args.TextView)) {
+                    return true;
+                }
             }
         }
 
@@ -59,43 +60,36 @@ public class AutoSurroundCommandHandler : ICommandHandler<TypeCharCommandArgs> {
     }
 
 
-    private void SurroundWith(char opening, char closing, ITextView textView) {
+    private bool SurroundWith(char opening, char closing, ITextView textView) {
+        List<(int Position, char Character)> edits;
         ITextUndoHistory history;
 
+
+        // Pair the start of the span with the opening character and the end of the
+        // span with the closing character, then sort the positions in descending order.
+        // We need to do this in reverse order, because every character that we insert
+        // will cause the points after it to no longer refer to the correct position.
+        edits = textView
+            .Selection
+            .SelectedSpans
+            .Where((x) => !x.IsEmpty)
+            .SelectMany((x) => new[] { (x.Start.Position, Character: opening), (x.End.Position, Character: closing) })
+            .OrderByDescending((x) => x.Position)
+            .ToList();
+
+        // If there are multiple selections, but they are all empty, the `Selection.IsEmpty` property
+        // returns true (which the caller uses as a quick test to see whether we should even attempt
+        // to make changes). We don't edit empty selections, so it's possible that we didn't actually
+        // make any changes. If that's the case, then we can just cancel the undo transaction and exit.
+        if (edits.Count == 0) {
+            return false;
+        }
 
         history = _textUndoHistoryRegistry.GetHistory(textView.TextBuffer);
 
         using (ITextUndoTransaction transaction = history.CreateTransaction($"{opening}Auto Surround{closing}")) {
-            IEnumerable<(int Position, char Character)> edits;
-            bool changed;
-
-
-            changed = false;
-
-            // Pair the start of the span with the opening character and the end of the
-            // span with the closing character, then sort the positions in descending order.
-            // We need to do this in reverse order, because every character that we insert
-            // will cause the points after it to no longer refer to the correct position.
-            edits = textView
-                .Selection
-                .SelectedSpans
-                .Where((x) => !x.IsEmpty)
-                .SelectMany((x) => new[] { (x.Start.Position, Character: opening), (x.End.Position, Character: closing) })
-                .OrderByDescending((x) => x.Position);
-
             foreach ((int Position, char Character) edit in edits) {
                 textView.TextBuffer.Insert(edit.Position, edit.Character.ToString());
-                changed = true;
-            }
-
-            // If there are multiple selections, but they are all empty,
-            // the `Selection.IsEmpty` property returns false. We don't
-            // edit empty selections, so it's possible that we didn't
-            // actually make any changes. If that's the case, then we
-            // can just cancel the undo transaction and exit.
-            if (!changed) {
-                transaction.Cancel();
-                return;
             }
 
             // Now change the selected spans to account for
@@ -150,6 +144,8 @@ public class AutoSurroundCommandHandler : ICommandHandler<TypeCharCommandArgs> {
 
             transaction.Complete();
         }
+
+        return true;
     }
 
 }
